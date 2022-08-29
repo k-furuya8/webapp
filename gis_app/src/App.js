9962b4ba1ad58be3c862e59ee8d0e4d736466733
kfuruya <kfuruya.817@gmail.com>
2022-08-29 21:20:44 +0900

    first commit

---
+++ gis_app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import "https://unpkg.com/elm-pep@1.0.6/dist/elm-pep.js";
+import main from './MainMap'
+
+function App() {
+  return (
+    <div>
+      aaaaa
+
+    </div>
+  );
+}
+
+export default App;
